271b7764f09f6291d317e76e7ea2fbaeed440298
gitgup <gups.singhs@gmail.com>
2023-10-11 12:31:05 -0700

    added tensorflow import

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense
 import wandb
@@ -28,6 +29,7 @@ class MyModel(Model):
     
 def createSimpleModel(weight):
     input_layer = tf.keras.Input(shape=(1,))
-    simple_model = Dense(1,use_bias = False,activation="relu",kernel_initializer=tf.keras.initializers.Constant(weight))(input_layer)
+    simple_model = Dense(1,use_bias = False,activation="relu",
+                         kernel_initializer=tf.keras.initializers.Constant(weight))(input_layer)
     model = Model(inputs=input_layer, outputs=simple_model)
     return model

ec33bc8f072d49a255bf201551ec7c989596511a
gitgup <gups.singhs@gmail.com>
2023-11-10 11:42:10 -0800

    deleted subclassing model method

--- models.py
+++ models.py
@@ -4,29 +4,6 @@ from tensorflow.keras.layers import Dense
 import wandb
 import config
 
-class MyModel(Model):
-    def __init__(self):
-        super(MyModel, self).__init__()
-        config = wandb.config
-        self.dense1 = Dense(config.layer_1, activation=config.activation_1)
-        
-        self.dense2 = Dense(config.layer_2, activation=config.activation_2)
-        
-        self.dense3 = Dense(config.layer_3, activation=config.activation_3)
-        
-        self.dense4 = Dense(config.output_layer, activation=config.output_activation)
-        
-    def call(self, x):
-        x = self.dense1(x)
-        
-        x = self.dense2(x)
-        
-        x = self.dense3(x)
-        
-        x = self.dense4(x)
-        
-        return x
-    
 def createSimpleModel(weight):
     input_layer = tf.keras.Input(shape=(1,))
     simple_model = Dense(1,use_bias = False,activation="relu",

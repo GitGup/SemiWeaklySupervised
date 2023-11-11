3986fa888b35726a573909ea387d205d04c5c9c8
gitgup <gups.singhs@gmail.com>
2023-11-11 00:42:17 -0800

    added train_wsmodel method for 2 param

--- models.py
+++ models.py
@@ -1,8 +1,4 @@
-import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense
-import wandb
-import config
+from common import *
 
 def createSimpleModel(weight):
     input_layer = tf.keras.Input(shape=(1,))
@@ -10,3 +6,69 @@ def createSimpleModel(weight):
                          kernel_initializer=tf.keras.initializers.Constant(weight))(input_layer)
     model = Model(inputs=input_layer, outputs=simple_model)
     return model
+
+def train_wsmodel(sigfrac, m1, m2, w1, w2, epochs, lr_schedule):
+
+    sig_list = []
+    w1_list = []
+    w2_list = []
+
+    fit_out = {}
+
+    epsilon = 1e-6
+
+    sigfrac = sigfrac
+    print("Signal Fraction: ", sigfrac)
+    for l in model_all_MSE.layers:
+        l.trainable=False
+
+    model3 = createSimpleModel(w1)
+    model32 = createSimpleModel(w2)
+
+    inputs_hold3 = tf.keras.Input(shape=(1,))
+    simple_model3 = tf.exp(Dense(1,use_bias = False,activation='linear',kernel_initializer=tf.keras.initializers.Constant(-1))(inputs_hold3))
+    model33 = Model(inputs = inputs_hold3, outputs = simple_model3)
+
+    inputs = tf.keras.Input(shape=(4,))
+    inputs2 = tf.keras.layers.concatenate([inputs,model3(tf.ones_like(inputs)[:,0]),model32(tf.ones_like(inputs)[:,0])])
+    hidden_layer_1 = model_all_MSE(inputs2)
+    LLR = hidden_layer_1 / (1.- hidden_layer_1 + epsilon)
+    LLR_xs = 1 + sigfrac * LLR - sigfrac
+    #LLR_xs = 1. + model33(tf.ones_like(inputs)[:,0]) * LLR
+    ws = (LLR_xs / (1.+ LLR_xs))
+    model_all2 = Model(inputs = inputs, outputs = ws)
+    model_all2.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=tf.keras.optimizers.Adam(learning_rate = lr_schedule))
+
+    m1 = m1
+    m2 = m2
+
+    test_background = int(1/2 *len(x[0,0]))
+    train_background = int(1/4 * len(x[0,0]))
+    train_data = int(1/4 * len(x[0,0]))
+    train_reference = int(1/4 * len(x[0,0]))
+    #signal
+    test_signal_length = int(1/2*len(x[m1,m2]))
+    N = int(1/4 * (len(x[0,0])))
+    signal = x[m1, m2][test_signal_length:test_signal_length + int(sigfrac*N)]
+
+    x_vals_ = np.concatenate([x[0,0][test_background:],signal])
+    #[reference (0), data_background (1), signal(1)]
+    y_vals_ = np.concatenate([np.zeros(train_reference),np.ones(train_data + 1),np.ones(len(signal))])
+
+    X_train_, X_val_, Y_train_, Y_val_ = train_test_split(x_vals_, y_vals_, test_size=0.5)
+
+    with tf.device('/GPU:0'):
+        for epoch in range(epochs):
+            myhistory_hack_ = model_all2.fit(X_train_[:,0:4], Y_train_, epochs=1,validation_data=(X_val_[:,0:4], Y_val_),batch_size=1024)
+            w1_out = model_all2.trainable_weights[0].numpy()[0][0]
+            w2_out = model_all2.trainable_weights[1].numpy()[0][0]
+            fit_out[epoch] = [w1_out, w2_out]
+
+    sig_list+=[sigfrac]
+    w1_list+=[model_all2.trainable_weights[0].numpy()[0][0]]
+    w2_list+=[model_all2.trainable_weights[1].numpy()[0][0]]
+    #w3_list+=[np.exp(model_all2.trainable_weights[2].numpy()[0][0])]
+
+    print(m1, m2,model_all2.trainable_weights[0].numpy()[0][0],model_all2.trainable_weights[1].numpy()[0][0])
+    
+    return fit_out

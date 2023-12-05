2691cc836b590fc8f26c8471528366966487a9f0
gitgup <gups.singhs@gmail.com>
2023-12-04 21:35:30 -0800

    methods for creating landscapes

---
+++ landscapes.py
@@ -0,0 +1,152 @@
+from common import *
+import os
+from IPython.display import display, clear_output
+from PIL import Image, ImageSequence
+
+mass_range = [0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6]
+
+#create loss landscape for specific s/b and mass pair
+#can loop over this function in the range of singal or different masses
+
+def loss_landscape_nofit(sigfrac, m1, m2, z):
+    
+    #create grid of points for the model to be evaluated at
+    #current resolutionis 12 * 12 = 144
+    grid_axes = []
+    for w1 in mass_range:
+        for w2 in mass_range:
+            grid_axes.append((w1, w2))
+            
+    w1_values, w2_values = zip(*grid_axes)
+
+    loss_values = list(z[sigfrac, m1, m2])
+    min_loss = min(loss_values)
+    max_loss = max(loss_values)
+
+    star1_coords = (m1, m2)  
+    star2_coords = (m2, m1)
+
+    normalized_loss = [(x - min_loss) / (max_loss - min_loss) for x in loss_values]
+    
+    h = plt.hist2d(w1_values, w2_values, bins=(12, 12), cmap='viridis', weights=normalized_loss)
+    plt.scatter(*star1_coords, c='red', marker='*', s=200, label='Star 1')
+    plt.scatter(*star2_coords, c='blue', marker='*', s=200, label='Star 2')
+    plt.colorbar(label='Loss (BCE)')
+
+    plt.xlabel('m1')
+    plt.ylabel('m2')
+    plt.title('(m1 = {} | m2 = {}) sigfrac: {:.4f}'.format(m1, m2, sigfrac))
+    return h
+
+def create_loss_landscape_6Features(model, m1, m2):
+    
+    Nfeatures = 6
+    #check if loss dictionary exists, if it does load it, if not create empty one
+    dir_path = os.getcwd()
+    file_name = f"z_allm1m2_{Nfeatures}Features.npy"
+    file_path = os.path.join(dir_path, file_name)
+    
+    if os.path.exists(file_path):
+        z = np.load(f"z_allm1m2_{Nfeatures}Features.npy", allow_pickle = True).item()
+    else:
+        print("Dictionary doesn't exist, creating one...")
+        z = {}
+    #varying sigfrac, fixed mass pair
+    
+    losses_list = []
+
+    epsilon = 1e-6
+    sig_space = np.logspace(-3, -1, 20)
+    for sig in sig_space:
+        print("Signal Fraction: ", sig)
+        #print(w1, w2)
+        for w1 in mass_range:
+            for w2 in mass_range:
+                sigfrac = sig
+
+                for l in model.layers:
+                    l.trainable=False
+
+                inputs_hold = tf.keras.Input(shape=(1,))
+                simple_model = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w1))(inputs_hold)
+                model3 = Model(inputs = inputs_hold, outputs = simple_model)
+
+                inputs_hold2 = tf.keras.Input(shape=(1,))
+                simple_model2 = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w2))(inputs_hold2)
+                model32 = Model(inputs = inputs_hold2, outputs = simple_model2)
+
+                inputs_hold3 = tf.keras.Input(shape=(1,))
+                simple_model3 = tf.exp(Dense(1,use_bias = False,activation='linear',kernel_initializer=tf.keras.initializers.Constant(-1))(inputs_hold3))
+                model33 = Model(inputs = inputs_hold3, outputs = simple_model3)
+
+                inputs = tf.keras.Input(shape=(Nfeatures,))
+                inputs2 = tf.keras.layers.concatenate([inputs,model3(tf.ones_like(inputs)[:,0]),model32(tf.ones_like(inputs)[:,0])])
+                hidden_layer_1 = model(inputs2)
+                LLR = hidden_layer_1 / (1.-hidden_layer_1 + epsilon)
+                LLR_xs = 1.+sigfrac*LLR - sigfrac
+                #LLR_xs = 1.+model33(tf.ones_like(inputs)[:,0])*LLR
+                ws = LLR_xs / (1.+LLR_xs+0.0001)
+                model_all2 = Model(inputs = inputs, outputs = ws)
+                model_all2.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate = 0.01))
+
+                m1 = m1
+                m2 = m2
+                
+                #if computed this mass pair, break
+                
+                key = (sigfrac,m1,m2)
+                if key in z:
+                    break
+
+                test_background = int(1/2 *len(x[0,0, qq]))
+                train_background = int(1/4 * len(x[0,0,qq]))
+                train_data = int(1/4 * len(x[0,0,qq]))
+                train_reference = int(1/4 * len(x[0,0,qq]))
+                #signal
+                test_signal_length = int(1/2*len(x[m1,m2,qq]))
+                sig_frac = sigfrac
+
+                #randomize signal events
+                #random_test_signal_length = random.randint(0, test_signal_length - 1)
+                N = int(1/4 * (len(x[0,0,qq])))
+                signal = x[m1, m2,qq][test_signal_length:test_signal_length + int(sigfrac*N)]
+
+                x_vals_ = np.concatenate([x[0,0,qq][test_background:],signal])
+                y_vals_ = np.concatenate([np.zeros(train_reference),np.ones(train_data),np.ones(len(signal))])
+
+                X_train_, X_val_, Y_train_, Y_val_ = train_test_split(x_vals_, y_vals_, test_size=0.5, random_state = 42)
+
+                loss = model_all2.evaluate(x_vals_, y_vals_, verbose = 0)
+                losses_list.append(loss)
+                
+        if key in z:
+            print("Landscape for m1 = {} ".format(m1) + "and " + "m2 = {} ".format(m2) +" already exists for " + "{}".format(sigfrac) + " signal fraction")
+        else:
+            z[sigfrac, m1, m2] = losses_list
+            losses_list = []
+            np.save(f"z_allm1m2_{NFeatures}Features", z)
+
+#animate loss landscape over different signal fractions
+def create_gif_nofit(m1, m2, z):
+    
+    output_directory = '2dhist_images'
+    os.makedirs(output_directory, exist_ok=True)
+    
+    sig_space = np.logspace(-3, -1 , 20)
+    
+    frames = []
+    for sb in sig_space:
+    
+        loss_landscape_nofit(sb, m1, m2, z)
+
+        image_path = os.path.join(output_directory, f'hist_{sigfrac}.png')
+        plt.savefig(image_path)
+        plt.close()
+        clear_output(wait=True)
+
+        # Append the image to the frames list
+        frames.append(Image.open(image_path))
+
+    # Create the final GIF that combines all frames
+    output_gif_filename = f'sigspace{m1}{m2}fixed.gif'
+    frames[0].save(output_gif_filename, save_all=True, append_images=frames[1:], duration=400, loop=0)

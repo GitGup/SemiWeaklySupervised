5986a4cc49da83438a8ea5f1a798a6edeebb5ae8
gitgup <gups.singhs@gmail.com>
2023-12-08 07:29:31 -0800

    added accuracy method

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from common import *
+import sys
 
 def createSimpleModel(weight):
     input_layer = tf.keras.Input(shape=(1,))
@@ -72,3 +73,71 @@ def train_wsmodel(model, sigfrac, m1, m2, w1, w2, epochs):
     print(m1, m2,model_all2.trainable_weights[0].numpy()[0][0],model_all2.trainable_weights[1].numpy()[0][0])
     
     return fit_out
+
+def eval_semiweakly(model, sigfrac, w1, w2, m1, m2):
+
+    for l in model.layers:
+        l.trainable=False
+
+    inputs_hold = tf.keras.Input(shape=(1,))
+    simple_model = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w1))(inputs_hold)
+    model3 = Model(inputs = inputs_hold, outputs = simple_model)
+
+    inputs_hold2 = tf.keras.Input(shape=(1,))
+    simple_model2 = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w2))(inputs_hold2)
+    model32 = Model(inputs = inputs_hold2, outputs = simple_model2)
+
+    inputs_hold3 = tf.keras.Input(shape=(1,))
+    simple_model3 = tf.exp(Dense(1,use_bias = False,activation='linear',kernel_initializer=tf.keras.initializers.Constant(-1))(inputs_hold3))
+    model33 = Model(inputs = inputs_hold3, outputs = simple_model3)
+
+    inputs = tf.keras.Input(shape=(Nfeatures,))
+    inputs2 = tf.keras.layers.concatenate([inputs,model3(tf.ones_like(inputs)[:,0]),model32(tf.ones_like(inputs)[:,0])])
+    hidden_layer_1 = model(inputs2)
+    LLR = hidden_layer_1 / (1.-hidden_layer_1 + epsilon)
+    LLR_xs = 1.+sigfrac*LLR - sigfrac
+    #LLR_xs = 1.+model33(tf.ones_like(inputs)[:,0])*LLR
+    ws = LLR_xs / (1.+LLR_xs+0.0001)
+    model_all2 = Model(inputs = inputs, outputs = ws)
+    model_all2.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate = 0.01))
+
+    m1 = m1
+    m2 = m2
+
+    test_background = int(1/2 *len(x[0,0]))
+    train_background = int(1/4 * len(x[0,0]))
+    train_data = int(1/4 * len(x[0,0]))
+    train_reference = int(1/4 * len(x[0,0]))
+    #signal
+    test_signal_length = int(1/2*len(x[m1,m2]))
+    sig_frac = sigfrac
+
+    #randomize signal events
+    #random_test_signal_length = random.randint(0, test_signal_length - 1)
+    N = int(1/4 * (len(x[0,0])))
+    signal = x[m1, m2][test_signal_length:test_signal_length + int(sigfrac*N)]
+
+    x_data_ = np.concatenate([x[0,0][test_background:],signal])
+    y_data_ = np.concatenate([np.zeros(train_reference),np.ones(train_data),np.ones(len(signal))])
+
+    X_train_, X_val_, Y_train_, Y_val_ = train_test_split(x_data_, y_data_, test_size=0.5, random_state = 42)
+    
+    with tf.device('/GPU:0'):
+        loss = model_all2.evaluate(X_val_, Y_val_, verbose = 0)
+    return loss
+
+def pred_accuracy(y_test, scores):
+    background_count, signal_count = 0, 0
+
+    predictions_list = []
+    for pred in scores:
+        #arbitrary cutoff of 0.5
+        if float(pred) > 0.5:
+            predictions_list.append(int(1))
+            signal_count+=1
+        elif float(pred) < 0.5:
+            predictions_list.append(int(0))
+            background_count+=1
+            
+    accuracy = np.mean(predictions_list == y_test)
+    return accuracy

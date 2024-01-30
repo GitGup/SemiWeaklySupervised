dbff3f26891bbf5176ab80e14cc2d3aa5c858d1e
gitgup <gups.singhs@gmail.com>
2024-01-30 14:02:10 -0800

    landscape method is dynamical manifold function viewing angle

--- plotting.py
+++ plotting.py
@@ -8,49 +8,46 @@ import os
 import seaborn as sea
 from scipy.interpolate import griddata
 from scipy.interpolate import RectBivariateSpline
+from mpl_toolkits.mplot3d import Axes3D
 
 sea.set(style="white")
 
 #plot landscape dynamically
-def loss_landscape_nofit(sigfrac, m1, m2, z):
-    
-    #create grid of points for the model to be evaluated at
-    #current resolution is 23 * 23 = 529
-    
+def loss_landscape_nofit(sigfrac, m1, m2, z, step=0.25):
     start = 0.5
     end = 6
-    step = 0.125
-
-    weight_list = np.arange(start, end + step, step)
+    step = step
     
-    grid_axes = []
-    for w1 in weight_list:
-        for w2 in weight_list:
-            grid_axes.append((w1, w2))
-            
+    weight_list = np.arange(start, end + step, step)
+    grid_axes = [(w1, w2) for w1 in weight_list for w2 in weight_list]
     w1_values, w2_values = zip(*grid_axes)
 
     loss_values = list(z[sigfrac, m1, m2])
     min_loss = min(loss_values)
     max_loss = max(loss_values)
+    normalized_loss = [(x - min_loss) / (max_loss - min_loss) for x in loss_values]
+    bins = int(np.sqrt(len(z[sigfrac, m1, m2])))
 
-    star1_coords = (m1, m2)  
+    star1_coords = (m1, m2)
     star2_coords = (m2, m1)
 
-    normalized_loss = [(x - min_loss) / (max_loss - min_loss) for x in loss_values]
-    
-    h = plt.hist2d(w1_values, w2_values, bins=(23,23), cmap='viridis', weights=normalized_loss)
+    plt.figure(figsize=(8, 6))
+    h = plt.hist2d(w1_values, w2_values, bins=(bins, bins), cmap='viridis', weights=normalized_loss)
     plt.scatter(*star1_coords, c='red', marker='*', s=200, label='Star 1')
     plt.scatter(*star2_coords, c='blue', marker='*', s=200, label='Star 2')
     plt.colorbar(label='Loss (BCE)')
-
     plt.xlabel('m1')
     plt.ylabel('m2')
     plt.title('6 Features (m1 = {} | m2 = {}) sigfrac: {:.4f}'.format(m1, m2, sigfrac))
+    plt.legend()
+    plt.show()
+    
     return h
 
 #Loss Landscape but 3D
-def create_3D_loss_manifold(sigfrac, m1, m2):
+#change elv and azim for viewing angle
+#step is resolution
+def create_3D_loss_manifold(sigfrac, m1, m2, elv, azim):
 
     start = 0.5
     end = 6
@@ -88,5 +85,5 @@ def create_3D_loss_manifold(sigfrac, m1, m2):
 
     ax.grid(False)
     
-    ax.view_init(elev=30, azim=10)
+    ax.view_init(elev=elv, azim=azim)
     plt.show()

948f2734d83ef5f73d36f92fdf81031e27013327
gitgup <gups.singhs@gmail.com>
2024-01-30 00:13:31 -0800

    compiled models

--- models.py
+++ models.py
@@ -8,72 +8,56 @@ def createSimpleModel(weight):
     model = Model(inputs=input_layer, outputs=simple_model)
     return model
 
-def train_wsmodel(model, sigfrac, m1, m2, w1, w2, epochs):
+#dedicated training on single mass pair
+def compile_supervised(feature_dims, m1, m2):
+    model_dedicated = Sequential()
+    model_dedicated.add(Dense(128, input_dim=feature_dims, activation='relu'))
+    model_dedicated.add(Dense(128, activation='relu'))
+    model_dedicated.add(Dense(128, activation='relu'))
+    model_dedicated.add(Dense(1, activation='sigmoid'))
+    model_dedicated.compile(loss="binary_crossentropy", optimizer='adam', metrics=['accuracy'])
+    
+    return model_dedicated
 
-    sig_list = []
-    w1_list = []
-    w2_list = []
+#CWOLA comparison
+def compile_CWOLA(feature_dims, m1, m2):
+    model_cwola = Sequential()
+    model_cwola.add(Dense(128, input_dim=feature_dims, activation='relu'))
+    model_cwola.add(Dense(128, activation='relu'))
+    model_cwola.add(Dense(128, activation='relu'))
+    model_cwola.add(Dense(1, activation='sigmoid'))
+    model_cwola.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model_cwola
 
-    fit_out = {}
+#SemiWeak Model
+def compile_SemiWeakly(feature_dims, m1, m2, w1, w2):
+    
+    inputs_hold = tf.keras.Input(shape=(1,))
+    simple_model = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w1))(inputs_hold)
+    model3 = Model(inputs = inputs_hold, outputs = simple_model)
 
-    epsilon = 1e-6
-
-    sigfrac = sigfrac
-    print("Signal Fraction: ", sigfrac)
-    for l in model.layers:
-        l.trainable=False
-
-    model3 = createSimpleModel(w1)
-    model32 = createSimpleModel(w2)
+    inputs_hold2 = tf.keras.Input(shape=(1,))
+    simple_model2 = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w2))(inputs_hold2)
+    model32 = Model(inputs = inputs_hold2, outputs = simple_model2)
 
     inputs_hold3 = tf.keras.Input(shape=(1,))
     simple_model3 = tf.exp(Dense(1,use_bias = False,activation='linear',kernel_initializer=tf.keras.initializers.Constant(-1))(inputs_hold3))
     model33 = Model(inputs = inputs_hold3, outputs = simple_model3)
 
-    inputs = tf.keras.Input(shape=(4,))
+    inputs = tf.keras.Input(shape=(feature_dims,))
     inputs2 = tf.keras.layers.concatenate([inputs,model3(tf.ones_like(inputs)[:,0]),model32(tf.ones_like(inputs)[:,0])])
-    hidden_layer_1 = model(inputs2)
-    LLR = hidden_layer_1 / (1.- hidden_layer_1 + epsilon)
-    LLR_xs = 1 + sigfrac * LLR - sigfrac
-    #LLR_xs = 1. + model33(tf.ones_like(inputs)[:,0]) * LLR
-    ws = (LLR_xs / (1.+ LLR_xs))
-    model_all2 = Model(inputs = inputs, outputs = ws)
-    model_all2.compile(loss=tf.keras.losses.BinaryCrossentropy(), optimizer=tf.keras.optimizers.Adam(learning_rate = 0.01))
 
-    m1 = m1
-    m2 = m2
+    #physics prior
+    hidden_layer_1 = model_qq(inputs2)
+    LLR = hidden_layer_1 / (1.-hidden_layer_1 + epsilon)
 
-    test_background = int(1/2 *len(x[0,0]))
-    train_background = int(1/4 * len(x[0,0]))
-    train_data = int(1/4 * len(x[0,0]))
-    train_reference = int(1/4 * len(x[0,0]))
-    #signal
-    test_signal_length = int(1/2*len(x[m1,m2]))
-    N = int(1/4 * (len(x[0,0])))
-    signal = x[m1, m2][test_signal_length:test_signal_length + int(sigfrac*N)]
+    #LLR_xs = 1.+sigfrac*LLR - sigfrac
+    LLR_xs = 1.+ model33(tf.ones_like(inputs)[:,0])*LLR
+    ws = LLR_xs / (1.+LLR_xs)
 
-    x_vals_ = np.concatenate([x[0,0][test_background:],signal])
-    #[reference (0), data_background (1), signal(1)]
-    y_vals_ = np.concatenate([np.zeros(train_reference),np.ones(train_data),np.ones(len(signal))])
-
-    X_train_, X_val_, Y_train_, Y_val_ = train_test_split(x_vals_, y_vals_, test_size=0.5)
-
-    with tf.device('/GPU:0'):
-        for epoch in range(epochs):
-            myhistory_hack_ = model_all2.fit(X_train_[:,0:4], Y_train_, epochs=1,validation_data=(X_val_[:,0:4], Y_val_),batch_size=1024)
-            w1_out = model_all2.trainable_weights[0].numpy()[0][0]
-            w2_out = model_all2.trainable_weights[1].numpy()[0][0]
-            fit_out[epoch] = [w1_out, w2_out]
-
-    sig_list+=[sigfrac]
-    w1_list+=[model_all2.trainable_weights[0].numpy()[0][0]]
-    w2_list+=[model_all2.trainable_weights[1].numpy()[0][0]]
-    #w3_list+=[np.exp(model_all2.trainable_weights[2].numpy()[0][0])]
-
-    print(m1, m2,model_all2.trainable_weights[0].numpy()[0][0],model_all2.trainable_weights[1].numpy()[0][0])
+    SemiWeakModel = Model(inputs = inputs, outputs = ws)
+    SemiWeakModel.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate = 0.01))
     
-    return fit_out
-
 def eval_semiweakly(model, sigfrac, w1, w2, m1, m2):
 
     for l in model.layers:

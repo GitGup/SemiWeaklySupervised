578df582134762236605557871d314f2cc5c49d0
gitgup <gups.singhs@gmail.com>
2024-01-30 02:02:16 -0800

    updated comileSemiWeakly method, added WeightConstraint class

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from common import *
 import sys
+from tensorflow.keras.constraints import Constraint
 
 def createSimpleModel(weight):
     input_layer = tf.keras.Input(shape=(1,))
@@ -29,15 +30,26 @@ def compile_CWOLA(feature_dims, m1, m2):
     model_cwola.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model_cwola
 
+class WeightConstraint(Constraint):
+    def __init__(self, min_value=0.5, max_value=6):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def __call__(self, weights):
+        return tf.clip_by_value(weights, self.min_value, self.max_value)
+
+    def get_config(self):
+        return {'min_value': self.min_value, 'max_value': self.max_value}
+
 #SemiWeak Model
-def compile_SemiWeakly(feature_dims, m1, m2, w1, w2):
+def compileSemiWeakly(feature_dims, params, m1, m2, w1, w2):
     
     inputs_hold = tf.keras.Input(shape=(1,))
-    simple_model = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w1))(inputs_hold)
+    simple_model = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w1), kernel_constraint=WeightConstraint())(inputs_hold)
     model3 = Model(inputs = inputs_hold, outputs = simple_model)
 
     inputs_hold2 = tf.keras.Input(shape=(1,))
-    simple_model2 = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w2))(inputs_hold2)
+    simple_model2 = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w2), kernel_constraint=WeightConstraint())(inputs_hold2)
     model32 = Model(inputs = inputs_hold2, outputs = simple_model2)
 
     inputs_hold3 = tf.keras.Input(shape=(1,))
@@ -51,12 +63,17 @@ def compile_SemiWeakly(feature_dims, m1, m2, w1, w2):
     hidden_layer_1 = model_qq(inputs2)
     LLR = hidden_layer_1 / (1.-hidden_layer_1 + epsilon)
 
-    #LLR_xs = 1.+sigfrac*LLR - sigfrac
-    LLR_xs = 1.+ model33(tf.ones_like(inputs)[:,0])*LLR
+    if params == 2:
+        LLR_xs = 1.+sigfrac*LLR - sigfrac
+    elif params == 3:
+        LLR_xs = 1. + model33(tf.ones_like(inputs)[:,0])*LLR
+    else:
+        print("Choose 2 or 3 parameters")
     ws = LLR_xs / (1.+LLR_xs)
 
     SemiWeakModel = Model(inputs = inputs, outputs = ws)
     SemiWeakModel.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate = 0.01))
+    return SemiWeakModel
     
 def eval_semiweakly(model, sigfrac, w1, w2, m1, m2):
 

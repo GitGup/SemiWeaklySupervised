39d41e7a3b529729f742187a6f716da8471f097c
gitgup <gups.singhs@gmail.com>
2024-02-13 22:55:33 -0800

    updated parametrized data dir

--- model_training.py
+++ model_training.py
@@ -10,25 +10,26 @@ import wandb
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 import os
+from utils import send_slack_message, send_slack_plot
 
 #training data
-x_data_qq = np.load("data/x_parametrized_data_qq.npy")
-y_data_qq = np.load("data/y_parametrized_data_qq.npy")
+x_data_qq = np.load("/pscratch/sd/g/gupsingh/x_parametrized_data_qq_fixed.npy")
+y_data_qq = np.load("/pscratch/sd/g/gupsingh/y_parametrized_data_qq_fixed.npy")
 X_train_qq, X_val_qq, Y_train_qq, Y_val_qq = train_test_split(x_data_qq, y_data_qq, test_size=0.5, random_state = 42)
 
 pscratch_dir = "/pscratch/sd/g/gupsingh/"
 os.environ["WANDB_DIR"] = pscratch_dir
 
 config = {
-    "layer_1_neurons": 1024,
-    "layer_2_neurons": 512,
-    "layer_3_neurons": 256,
+    "layer_1_neurons": 256,
+    "layer_2_neurons": 128,
+    "layer_3_neurons": 64,
     "output_neurons": 1,
     "activation": "relu",
     "output_activation": "sigmoid",
     "optimizer": "adam",
     "learning_rate": 0.01,
-    "loss": "binary_crossentropy",
+    "loss": "MeanSquaredError",
     "epochs": 1,
     "batch_size": 1024
 }
@@ -58,25 +59,29 @@ def train_parametrized(X_train, Y_train, X_val, Y_val, config, return_history=Fa
         history_parametrized = model_parametrized.fit(X_train, Y_train, epochs=config.epochs, validation_data=(X_val, Y_val), batch_size=config.batch_size, callbacks=[es, WandbCallback()])
         
     if return_history:
-        return model_parametrized, history_parametrized.history
+        return model_parametrized, history_parametrized
     else:
         return model_parametrized
 
 model_parametrized, history_parametrized = train_parametrized(X_train_qq, Y_train_qq, X_val_qq, Y_val_qq, config, return_history = True)
 model_parametrized.save(pscratch_dir + run_name)
 
-SLACK_API_TOKEN = "xoxb-327063347744-6595347520051-z1j3XpfctTSv05EQCQCbHgRP"
-client = WebClient(token=SLACK_API_TOKEN)
-channel_id = "D05JLSUNH8V"
-
-message = "Parametrized Training finished running!"
-
-try:
-    response = client.chat_postMessage(channel=channel_id, text=message)
-    print("Message sent successfully:", response["ts"])
-except SlackApiError as e:
-    print("Error sending message:", e.response["error"])
-
 wandb.finish()
-print("DONE TRAINING")
 
+num_epochs_trained = len(history_parametrized.history['loss'])
+val_accuracy = history_parametrized['val_accuracy']
+
+#Diagonistic Plot
+plt.figure()
+plt.plot(history_parametrized['loss'], label='Training Loss')
+plt.plot(history_parametrized['val_loss'], label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Training and Validation Loss')
+img_path = "plots/parametrized_trainingloss.png"
+plt.savefig(img_path)
+plt.legend()
+plt.show()
+
+send_slack_message(f"Done Training - Epochs: {num_epochs_trained} - Validation Accuracy: {val_accuracy}")
+send_slack_plot(img_path)

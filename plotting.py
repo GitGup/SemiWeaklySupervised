eb85a810544fd07dd9eae3e1d6b393c5fbc64f39
gitgup <gups.singhs@gmail.com>
2024-02-13 22:50:30 -0800

    added plotting both landscapes method

--- plotting.py
+++ plotting.py
@@ -9,7 +9,7 @@ from mpl_toolkits.mplot3d import Axes3D
 sea.set(style="white")
 
 #plot landscape dynamically
-def loss_landscape_nofit(sigfrac, m1, m2, z, step=0.25):
+def loss_landscape_nofit(sigfrac, m1, m2, z, step=0.25, save = False):
     start = 0.5
     end = 6
     step = step
@@ -38,12 +38,15 @@ def loss_landscape_nofit(sigfrac, m1, m2, z, step=0.25):
     plt.legend()
     plt.show()
     
+    if save == True:
+        plt.savefig(f'plots/landscape{float(m1)}{float(m2)}.png', dpi=450, bbox_inches='tight')
+    
     return h
 
 #Loss Landscape but 3D
 #change elv and azim for viewing angle
 #step is resolution
-def create_3D_loss_manifold(sigfrac, m1, m2, z, step, elev, azim):
+def create_3D_loss_manifold(sigfrac, m1, m2, z, step, elev, azim, save = False):
 
     start = 0.5
     end = 6
@@ -87,11 +90,13 @@ def create_3D_loss_manifold(sigfrac, m1, m2, z, step, elev, azim):
     ax.set_zticks([])
     
     ax.view_init(elev=elev, azim=azim)
-    #plt.savefig('plot.png', dpi=450, bbox_inches='tight')
+    
+    if save == True:
+        plt.savefig(f'plots/manifold{float(m1)}{float(m2)}.png', dpi=450, bbox_inches='tight')
     return ax
 
 #interpolating using scipy RectBivariateSpline
-def plot_interpolated_landscape(sigfrac, m1, m2, z, step):
+def plot_interpolated_landscape(sigfrac, m1, m2, z, step, save = False):
     
     start = 0.5
     end = 6
@@ -143,5 +148,53 @@ def plot_interpolated_landscape(sigfrac, m1, m2, z, step):
     ax[1].set_ylabel(r"$w_{2}$")
 
     plt.tight_layout()
-    #plt.savefig('interpolation3.55.5.png', dpi=450, bbox_inches='tight')
+    if save == True:
+        plt.savefig(f'plots/interpolation{float(m1)}{float(m2)}.png', dpi=450, bbox_inches='tight')
     return fig
+
+def plot_landscapes(sigfrac, m1, m2, z, a, step, save = False):
+    start = 0.5
+    end = 6
+    step = step
+
+    #weightspace
+    weight_list = np.arange(start, end + step, step)
+    grid_axes = [(w1, w2) for w1 in weight_list for w2 in weight_list]
+    w1_values, w2_values = zip(*grid_axes)
+
+    #loss
+    loss_values = list(z[sigfrac, m1, m2])
+    min_loss = min(loss_values)
+    max_loss = max(loss_values)
+    
+    AUC_values = list(a[sigfrac, m1, m2])
+    min_loss = min(AUC_values)
+    max_loss = max(AUC_values)
+    bins = int(np.sqrt(len(a[sigfrac, m1, m2])))
+
+    star1_coords = (m1, m2)
+    star2_coords = (m2, m1)
+
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+    ax[0].hist2d(w1_values, w2_values, bins = (bins, bins), cmap='viridis', weights=loss_values)
+    ax[0].set_aspect("equal")
+    ax[0].set_title(f"Loss Landscape")
+    ax[0].set_xlabel(r"$w_{1}$")
+    ax[0].set_ylabel(r"$w_{2}$")
+    ax[0].scatter(*star1_coords, c='red', marker='*', s=200, label='Star 1')
+    ax[0].scatter(*star2_coords, c='blue', marker='*', s=200, label='Star 2')
+    
+    ax[1].hist2d(w1_values, w2_values, bins = (bins, bins), cmap='viridis', weights=AUC_values)
+    ax[1].set_aspect("equal")
+    ax[1].set_title("AUC Landscape")
+    ax[1].set_xlabel(r"$w_{1}$")
+    ax[1].set_ylabel(r"$w_{2}$")
+    ax[1].scatter(*star1_coords, c='red', marker='*', s=200, label='Star 1')
+    ax[1].scatter(*star2_coords, c='blue', marker='*', s=200, label='Star 2')
+
+    plt.subplots_adjust(top=0.95)
+    #plt.tight_layout()
+    fig.suptitle(f"$m_{1}: ${m1 * 100} $m_{2}: {m2 * 100}$")
+    if save == True:
+        plt.savefig(f'plots/bothlandscape{float(m1)}{float(m2)}.png', dpi=450, bbox_inches='tight')
+    return ax

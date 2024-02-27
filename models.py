8a92ac308960a54286aa3cea8632b92f466c91fa
gitgup <gups.singhs@gmail.com>
2024-02-26 19:46:05 -0800

    fpr cut on train_supervised, 3 prong added

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from data import load_data
 
 qq = "qq"
 qqq = "qqq"
+
 class WeightConstraint(Constraint):
     def __init__(self, min_value=0.5, max_value=6):
         self.min_value = min_value
@@ -23,9 +24,10 @@ def simpleModel(weight):
     model = Model(inputs=input_layer, outputs=simple_model)
     return model
 
-def train_supervised(feature_dims, m1, m2, noise = False):
-    qq = "qq"
-    x = load_data("data/x_array_qqq.npy", noise_dims = 0)
+#dedicated training on single mass pair
+es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+def train_supervised(feature_dims, m1, m2, fpr_cut = 1e-4):
+    epsilon = 1e-4
     x_data_full = np.concatenate([x[0,0, qq, noise],x[m1,m2, qq, noise]])
     y_data_full = np.concatenate([np.zeros(len(x[0,0, qq, noise])),np.ones(len(x[m1,m2, qq, noise]))])
     X_train_full, X_val_full, Y_train_full, Y_val_full = train_test_split(x_data_full, y_data_full, test_size=0.5, random_state = 42)
@@ -37,8 +39,12 @@ def train_supervised(feature_dims, m1, m2, noise = False):
     model_dedicated.add(Dense(1, activation='sigmoid'))
     model_dedicated.compile(loss="binary_crossentropy", optimizer='adam', metrics=['accuracy'])
 
-    history_fullySupervised = model_dedicated.fit(X_train_full, Y_train_full, epochs=100,validation_data=(X_val_full, Y_val_full),batch_size=1024, verbose = 0)
-    return model_dedicated
+    history_fullySupervised = model_dedicated.fit(X_train_full, Y_train_full, epochs=200,validation_data=(X_val_full, Y_val_full),batch_size=1024, verbose = 0, callbacks = [es])
+    preds = model_dedicated.predict(X_val_full,batch_size=1000)
+    fpr, tpr, _ = metrics.roc_curve(Y_val_full, preds)
+    adjusted_fpr = np.maximum(fpr, fpr_cut)
+    maxsic = np.max(tpr / np.sqrt(adjusted_fpr + epsilon))
+    return maxsic
 
 #CWOLA comparison
 def compileCWOLA(feature_dims, m1, m2):
@@ -52,7 +58,7 @@ def compileCWOLA(feature_dims, m1, m2):
 
 epsilon = 1e-4
 #SemiWeak Model
-def compileSemiWeakly(model, feature_dims, params, m1, m2, w1, w2):
+def compileSemiWeakly(sigfrac, model, feature_dims, params, m1, m2, w1, w2):
     
     inputs_hold = tf.keras.Input(shape=(1,))
     simple_model = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w1), kernel_constraint=WeightConstraint())(inputs_hold)
@@ -74,8 +80,7 @@ def compileSemiWeakly(model, feature_dims, params, m1, m2, w1, w2):
     LLR = hidden_layer_1 / (1.-hidden_layer_1 + epsilon)
 
     if params == 2:
-        print("test")
-        #LLR_xs = 1.+sigfrac*LLR - sigfrac
+        LLR_xs = 1.+sigfrac*LLR - sigfrac
     elif params == 3:
         LLR_xs = 1. + model33(tf.ones_like(inputs)[:,0])*LLR - model33(tf.ones_like(inputs)[:,0])
     else:
@@ -85,3 +90,43 @@ def compileSemiWeakly(model, feature_dims, params, m1, m2, w1, w2):
     SemiWeakModel = Model(inputs = inputs, outputs = ws)
     SemiWeakModel.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate = 0.01))
     return SemiWeakModel
+
+def compileSemiWeakly3Prong(sigfrac, model_qq, model_qqq, feature_dims, parameters, m1, m2, w1, w2):
+    #freeze both two pronged and three pronged prior models
+    for l in model_qq.layers:
+        l.trainable=False
+
+    for l in model_qqq.layers:
+        l.trainable=False
+
+    inputs_hold = tf.keras.Input(shape=(1,))
+    simple_model = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w1), kernel_constraint=WeightConstraint())(inputs_hold)
+    model3 = Model(inputs = inputs_hold, outputs = simple_model)
+
+    inputs_hold2 = tf.keras.Input(shape=(1,))
+    simple_model2 = Dense(1,use_bias = False,activation='relu',kernel_initializer=tf.keras.initializers.Constant(w2), kernel_constraint=WeightConstraint())(inputs_hold2)
+    model32 = Model(inputs = inputs_hold2, outputs = simple_model2)
+
+    inputs_hold3 = tf.keras.Input(shape=(1,))
+    simple_model3 = tf.exp(Dense(1,use_bias = False,activation='linear',kernel_initializer=tf.keras.initializers.Constant(-1))(inputs_hold3))
+    model33 = Model(inputs = inputs_hold3, outputs = simple_model3)
+
+    inputs_hold4 = tf.keras.Input(shape=(1,))
+    simple_model4 = Dense(1,use_bias = False,activation='sigmoid',kernel_initializer=tf.keras.initializers.Constant(1.))(inputs_hold4)
+    model34 = Model(inputs = inputs_hold4, outputs = simple_model4)
+
+    inputs = tf.keras.Input(shape=(6,))
+    inputs2 = tf.keras.layers.concatenate([inputs,model3(tf.ones_like(inputs)[:,0]),model32(tf.ones_like(inputs)[:,0])])
+    hidden_layer_1 = model_qq(inputs2)
+    hidden_layer_13 = model_qqq(inputs2)
+    LLR2 = hidden_layer_1 / (1.-hidden_layer_1+0.0001)
+    LLR3 = hidden_layer_13 / (1.-hidden_layer_13+0.0001)
+    
+    if parameters == 2:
+        LLR_xs = 1.+ sigfrac*LLR3 - sigfrac
+    if parameters == 4:
+        LLR_xs_fixed = 1 + model33(tf.ones_like(inputs)[:,0])*model34(tf.ones_like(inputs)[:,0]) * LLR3 + (1-model34(tf.ones_like(inputs)[:,0]))*LLR2*model33(tf.ones_like(inputs)[:,0]) - model33(tf.ones_like(inputs)[:,0])
+    ws = LLR_xs_fixed / (1.+LLR_xs_fixed+0.0001)
+    SemiWeak3Prong = Model(inputs = inputs, outputs = ws)
+    SemiWeak3Prong.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate = lr_schedule))
+    return SemiWeak3Prong

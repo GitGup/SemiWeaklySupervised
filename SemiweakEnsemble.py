f84a16d3b15c792a6105c2ce7cae9177322f123e
gitgup <gups.singhs@gmail.com>
2024-02-27 17:21:53 -0800

    extra QCD script working

--- SemiweakEnsemble.py
+++ SemiweakEnsemble.py
@@ -16,7 +16,7 @@ from utils import send_slack_message, send_slack_plot, get_stuck_weights
 x = load_data("/pscratch/sd/g/gupsingh/x_array_fixed_EXTRAQCD.pkl", noise_dims = 0)
 x_data_qq = np.load("/pscratch/sd/g/gupsingh/x_parametrized_data_qq_extra.npy")
 y_data_qq = np.load("/pscratch/sd/g/gupsingh/y_parametrized_data_qq_extra.npy")
-model_path = "/pscratch/sd/g/gupsingh/volcanic-yogurt-69"
+model_path = "/pscratch/sd/g/gupsingh/eager-rain-75"
 model_qq = tf.keras.models.load_model(model_path)
 #es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 
@@ -27,6 +27,12 @@ model_qq = tf.keras.models.load_model(model_path)
 qq = "qq"
 noise = False
 epsilon = 1e-4
+
+from utils import get_stuck_weights
+
+noise = False
+epsilon = 1e-4
+#es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 
 def train_semiweak(feature_dims, m1, m2, parameters, injections, m_initializations, decay = "qq"):
     maxsicandstd1 = {}
@@ -44,7 +50,7 @@ def train_semiweak(feature_dims, m1, m2, parameters, injections, m_initializatio
     initial_weights_runs = []
     tuple_rates_semiweak = {}
     tuple_rates_weak = {}
-    
+
     qq = decay
     test_signal = int(1/2*len(x[m1,m2, qq, noise]) + 1)
 
@@ -108,13 +114,13 @@ def train_semiweak(feature_dims, m1, m2, parameters, injections, m_initializatio
 
                 history_semiweak = model_semiweak.fit(X_train_[:,0:feature_dims], Y_train_, epochs=50,
                                                        validation_data=(X_val_[:,0:feature_dims], Y_val_),batch_size=1024, verbose = 0)
-                
+
                 print(f"m1: {m1}",f"m2: {m2}", f"w1: {model_semiweak.trainable_weights[0].numpy()[0][0]}", f"w2: {model_semiweak.trainable_weights[1].numpy()[0][0]}")
 
                 weight_list1_kruns+=[model_semiweak.trainable_weights[0].numpy()[0][0]]
                 weight_list2_kruns+=[model_semiweak.trainable_weights[1].numpy()[0][0]]
                 weight_list3_kruns+=[model_semiweak.trainable_weights[2].numpy()[0][0]]
-                
+
                 if decay == "qqq":
                     weight_list4_kruns+=[model_semiweak.trainable_weights[3].numpy()[0][0]]
 
@@ -129,45 +135,45 @@ def train_semiweak(feature_dims, m1, m2, parameters, injections, m_initializatio
             weight_list3_injections.append(weight_list3_kruns)
             if decay == "qqq":
                 weight_list4_injections.append(weight_list4_kruns)
-            
+
             #now average over the k classifiers runs
             y = np.concatenate([np.zeros(test_background),np.ones(test_signal)])
-            
+
             #get the lowest scores from dictionary of losses and scores
             top_items = sorted(scoreLossdict.items())[:3]
             lowest_losses = [x[0] for x in top_items]
             top_scores = [scoreLossdict[loss] for loss in lowest_losses]
             fpr, tpr, _ = metrics.roc_curve(y, np.median(top_scores, axis = 0))
             tuple_rates_semiweak[(sigfrac, injection)] = (fpr, tpr)
-            
+
             epsilon = 1e-4
             msic1_kmedian = np.max(tpr/np.sqrt(fpr+epsilon))
             score1_injections.append(score1_kruns)
             msic1_median.append(msic1_kmedian)
             print(f" --- msic1_median on injection {injection}: {msic1_kmedian} ---")
-            
+
             #weakly supervised only inject no k classifiers (out of mass initialization loop) no averaging to be done yet
             model_CWOLA = compileCWOLA(feature_dims, m1, m2)
-            history_CWOLA = model_CWOLA.fit(X_train_[:,0:feature_dims], Y_train_, epochs=20,validation_data=(X_val_[:,0:feature_dims], Y_val_),batch_size=1024, verbose = 0)
-            
+            history_CWOLA = model_CWOLA.fit(X_train_[:,0:feature_dims], Y_train_, epochs=50,validation_data=(X_val_[:,0:feature_dims], Y_val_),batch_size=1024, verbose = 0)
+
             scores2 = model_CWOLA.predict(np.concatenate([x[0,0, qq, noise][0:test_background],x[m1,m2, qq, noise][0:test_signal]]),batch_size=1024)
             score2_injections.append(scores2)
-            
+
             y2 = np.concatenate([np.zeros(test_background),np.ones(test_signal)])
             fpr2, tpr2, _ = metrics.roc_curve(y2, scores2)
             msic2.append(np.max(tpr2/np.sqrt(fpr2+epsilon)))
             tuple_rates_weak[(sigfrac, injection)] = (fpr2, tpr2)
-        
+
         msic1_runs.append(np.median(msic1_median))
         print(f" --- msic1_runs for signal fraction {sigfrac}: {np.median(msic1_median)} ---")
         std1_runs.append(np.std(msic1_median))
         print(f" --- msic1_runs_std for signal fraction {sigfrac}: {np.std(msic1_median)} ---")
-        
+
         print(f" --- msic2_runs for signal fraction {sigfrac}: {np.median(msic2)} ---")
         std2_runs.append(np.std(msic2))
         print(f" --- msic2_runs_std for signal fraction {sigfrac}: {np.std(msic2)} ---")
         msic2_runs.append(np.median(msic2))
-        
+
         score1_injections_raw_runs.append(score1_injections)
         score2_injections_raw_runs.append(score2_injections)
         weight_list1_runs.append(weight_list1_injections)
@@ -175,20 +181,22 @@ def train_semiweak(feature_dims, m1, m2, parameters, injections, m_initializatio
         weight_list3_runs.append(weight_list3_injections)
         if decay == "qqq":
             weight_list4_runs.append(weight_list4_injections)
-        
+
         maxsicandstd1[sigfrac] = (np.median(msic1_median), np.std(msic1_median))
         maxsicandstd2[sigfrac] = (np.median(msic2), np.std(msic2))
-        if len(x[0,0,qq,noise]) > 121352:
+        
+        if len(x[0,0, qq, noise]) > 121352:
             extra = True
+            
         extra_str = "_extra" if extra else ""
         np.save(f"data/maxsicandstd1_script{float(m1)}{float(m2)}_{decay}{extra}.npy", maxsicandstd1)
         np.save(f"data/maxsicandstd2_script{float(m1)}{float(m2)}_{decay}{extra}.npy", maxsicandstd2)
-        
+
     stuck_weights = get_stuck_weights(sigspace, injections, m_initializations, m1, m2, weight_list1_runs, weight_list2_runs, decay)
-    np.save(f"data/stuck_weights{float(m1)}{float(m2)}_{decay}{extra}.npy", stuck_weights)
-    np.save(f"data/tuplerates{float(m1)}{float(m2)}_{decay}{extra}.npy", tuple_rates_semiweak)
-    np.save(f"data/tuplerates2{float(m1)}{float(m2)}_{decay}{extra}.npy", tuple_rates_weak)
-    
+    np.save(f"data/stuck_weights_script{float(m1)}{float(m2)}_{decay}{extra}.npy", stuck_weights)
+    np.save(f"data/tuplerates_script{float(m1)}{float(m2)}_{decay}{extra}.npy", tuple_rates_semiweak)
+    np.save(f"data/tuplerates2_script{float(m1)}{float(m2)}_{decay}{extra}.npy", tuple_rates_weak)
+
     np.save(f"data/msic1_median_script{float(m1)}{float(m2)}_{decay}{extra}.npy", maxsicandstd1)
     np.save(f"data/msic2_median_script{float(m1)}{float(m2)}_{decay}{extra}.npy", maxsicandstd2)
     np.save(f"data/std1_median_script{float(m1)}{float(m2)}_{decay}{extra}.npy", std1_runs)
@@ -196,7 +204,7 @@ def train_semiweak(feature_dims, m1, m2, parameters, injections, m_initializatio
     np.save(f"data/weight_list1_runs_script{float(m1)}{float(m2)}_{decay}{extra}.npy", weight_list1_runs)
     np.save(f"data/weight_list2_runs_script{float(m1)}{float(m2)}_{decay}{extra}.npy", weight_list2_runs)
     np.save(f"data/weight_list3_runs_script{float(m1)}{float(m2)}_{decay}{extra}.npy", weight_list3_runs)
-    
+
     np.save(f"data/score1_injections_raw_runs{float(m1)}{float(m2)}_{decay}{extra}.npy", score1_injections_raw_runs)
     np.save(f"data/score2_injections_raw_runs{float(m1)}{float(m2)}_{decay}{extra}.npy", score2_injections_raw_runs)
     if decay == "qqq":
